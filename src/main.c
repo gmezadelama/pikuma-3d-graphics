86e3ecaa4c3a50a1513c2d7f335914f5b406add2
Gustavo MDL <Admin@Administrators-MacBook-Pro.local>
2020-04-24 19:11:53 -0400

    Multiple Header Files section

--- src/main.c
+++ src/main.c
@@ -2,52 +2,9 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <SDL2/SDL.h>
+#include "display.h"
 
 bool is_running = false;
-
-SDL_Window* window;
-SDL_Renderer* renderer;
-
-uint32_t* color_buffer = NULL;
-SDL_Texture* color_buffer_texture = NULL;
-
-int window_width = 800;
-int window_height = 600;
-
-bool initialize_window(void) {
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		fprintf(stderr, "Error initializing SDL.\n");
-		return false;
-	}
-
-	SDL_DisplayMode display_mode;
-	SDL_GetCurrentDisplayMode(0, &display_mode);
-	window_width = display_mode.w;
-	window_height = display_mode.h;
-
-	// Create a SDL Window
-	window = SDL_CreateWindow(
-		NULL,
-		SDL_WINDOWPOS_CENTERED,
-		SDL_WINDOWPOS_CENTERED,
-		window_width,
-		window_height,
-		SDL_WINDOW_BORDERLESS
-	);
-	if (!window) {
-		fprintf(stderr, "Error creating SDL window.\n");
-		return false;
-	}
-
-	// Create a SDL renderer
-	renderer = SDL_CreateRenderer(window, -1, 0);
-	if (!renderer) {
-		fprintf(stderr, "Error creating SDL renderer.\n");
-		return false;
-	}
-
-	return true;
-}
 
 void setup(void) {
 	// Allocate the requiered bytes in memory for the color buffer
@@ -81,47 +38,6 @@ void process_input(void) {
 
 void update(void) {}
 
-void setPixelColorAt(uint32_t color, int x, int y) {
-	color_buffer[(window_width * y) + x] = color;
-}
-
-void render_color_buffer(void) {
-	SDL_UpdateTexture(
-		color_buffer_texture,
-		NULL,
-		color_buffer,
-		(int)(window_width * sizeof(uint32_t))
-	);
-	SDL_RenderCopy(renderer, color_buffer_texture, NULL, NULL);
-}
-
-void clear_color_buffer(uint32_t color) {
-	for (int y = 0; y < window_height; y++) {
-		for (int x = 0; x < window_width; x++) {
-			setPixelColorAt(color, x, y);
-		}
-	}
-}
-
-void draw_grid(void) {
-	for (int y = 0; y < window_height; y++) {
-		for (int x = 0; x < window_width; x++) {
-			if ((x % 10 == 0) ||
-					(y % 10 == 0)) {
-				setPixelColorAt(0xFF447BBE, x, y);
-			}
-		}
-	}
-}
-
-void draw_rectangle(int x0, int y0, int width, int height, uint32_t color) {
-	for (int y = y0; y < y0 + height; y++) {
-		for (int x = x0; x < x0 + width; x++) {
-			setPixelColorAt(color, x, y);
-		}
-	}
-}
-
 void render(void) {
 	SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
 	SDL_RenderClear(renderer);
@@ -130,19 +46,12 @@ void render(void) {
 
 	int rectangle_width = 400;
 	int rectangle_height = 200;
-	draw_rectangle(window_width / 2 - rectangle_width / 2, window_height / 2 - rectangle_height / 2, rectangle_width, rectangle_height, 0xFF2B3784);
+	draw_rect(window_width / 2 - rectangle_width / 2, window_height / 2 - rectangle_height / 2, rectangle_width, rectangle_height, 0xFF2B3784);
 
 	render_color_buffer();
 	clear_color_buffer(0xFFB11313);
 
 	SDL_RenderPresent(renderer);
-}
-
-void destroy_window(void) {
-	free(color_buffer);
-	SDL_DestroyRenderer(renderer);
-	SDL_DestroyWindow(window);
-	SDL_Quit();
 }
 
 int main(void) {
